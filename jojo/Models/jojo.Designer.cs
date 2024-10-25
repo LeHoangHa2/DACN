4bdf25243121cd01fc8aeb162650e80c2fe151fd
Hale <nervergone111@gmail.com>
2024-10-25 23:59:15 +0700

    push code

--- jojo/Models/jojo.Designer.cs
+++ jojo/Models/jojo.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\thach\Downloads\jojo2\jojo\jojo\Models\jojo.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\dotha\OneDrive\Desktop\NNNNN\thuongmaidientu2021\jojo\Models\jojo.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
